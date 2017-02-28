3e264b301a5f1479e014feb34a494946356e9716
James Bomysoad <jbomyso1.binghamton.edu>
2017-02-27 21:23:21 -0500

    Worked on map.hpp

--- map.hpp
+++ map.hpp
@@ -9,21 +9,41 @@ template<typename Key_T, typename Mapped_T>
 class Map
 {
     public:
-        Map();
-        Map(const Map&);
-        Map &operator=(const Map &);
+        typedef std::pair<const Key_T, Mapped_T> value_type;
+        struct SNode {
+            SNode *next, *prev;
+        };
+  
+        struct Node: public SNode {
+            Node(value_type);
+            std::vector<Node> forward;
+        };
+  
+        // create empty map
+        Map(): numElements(0), node(nullptr) {};
+  
+        // Copy Constructor
+        Map(const Map &mp) {
+            Node ptr = new Node;
+            *ptr = *mp.node;
+        };
+       
+        // Copy assignment operator 
+        Map &operator=(const Map &mp) {
+            if(&mp == this)
+                return *this;
+
+            std::copy(
+        };
+
+
         Map(std::initializer_list<std::pair<const Key_T, Mapped_T> >);
         ~Map();
         size_t size() const;
         bool empty() const;
         
     private:
-        struct Node {
-            Key_T key;
-            Mapped_T val;
-
-            std::vector<Node> nextNode;
-        };
+        Node* node;
         size_t numElements;
 };
 
